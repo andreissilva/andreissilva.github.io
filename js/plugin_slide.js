4cad0531aae9c4e57cbef0c4927e40ccd7792423
andreissilva <andrei_walk@hotmail.com>
2017-06-05 16:31:02 -0300

    Add files via upload

---
+++ js/plugin_slide.js
@@ -0,0 +1,19 @@
+$(function(){
+			$('#banner').cycle({
+            fx: 'scrollRight',
+            speed: 2000,
+            timeout: 5000,
+            next: '#prox',
+            prev: '#ant',
+            pager: '#pager'
+        })
+    })
+	//INSERCAO DA FUNCAO DO BANNER DA PROMOCAO - 30/04/2015
+$(function(){
+			$('#corpo #propaganda #promo').cycle({
+            fx: 'scrollRight',
+            speed: 2000,
+            timeout: 5000,
+            
+        })
+    })	
